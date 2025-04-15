097f661db54e1ec078a22a767daa4eff3de71d14
kishan <35022263+patel24kishan@users.noreply.github.com>
2025-04-14 21:08:03 -0300

    update constant index

--- src/data/constants.js
+++ src/data/constants.js
@@ -158,7 +158,7 @@ export const skills = [
 
 export const experiences = [
   {
-    id: 0,
+    id: 96,
     img: "https://images.squarespace-cdn.com/content/v1/662d86830e0ed705ae9658fc/4ed9ff16-ca52-4d86-92db-ef38d10053eb/Astro+Games_Logo+Only.png?format=1500w",
     role: "Game Developer",
     company: "Astro Game Studio",
@@ -176,7 +176,7 @@ export const experiences = [
     doc: "",
   },
   {
-    id: 1,
+    id: 97,
     img: "https://helpusdefend.com/wp-content/uploads/2023/05/cropped-DEFEND_3D_logo_with_glow-1.png",
     role: "Game Developer",
     company: "HelpUpDefend",
@@ -194,7 +194,7 @@ export const experiences = [
     doc: "",
   },
   {
-    id: 2,
+    id: 98,
     img: "https://www.svgrepo.com/show/349407/ibm.svg",
     role: "Application Developer",
     company: "IBM Canada",
@@ -212,7 +212,7 @@ export const experiences = [
     doc: "",
   },
   {
-    id: 3,
+    id: 99,
     img: "https://media.licdn.com/dms/image/C560BAQFi2lc_IOfaGQ/company-logo_200_200/0/1656594783502/achievers_logo?e=1717027200&v=beta&t=BJrwNA3XKPSsOK7E8HB5g2zMJnVQdIpoNJmKIWfEfqc",
     role: "Software Developer Intern",
     company: "Achievers",
@@ -222,7 +222,7 @@ export const experiences = [
     doc: "",
   },
   {
-    id: 4,
+    id: 100,
     img: "https://media.glassdoor.com/sql/1759068/escrow-infotech-squarelogo-1559125938891.png",
     role: "Game Developer",
     company: "Xsquad Studios by Escrow Infotech",
