025658daa99cb40e959c247f4357b0eb62f2ec2b
kishan <35022263+patel24kishan@users.noreply.github.com>
2025-05-18 15:26:10 -0300

    update grammar ofr I am a in profile

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,6 +1,6 @@
 export const Bio = {
   name: "Kishan",
-  roles: ["Developer", "Programmer", "Engineer", "Freelancer"],
+  roles: ["a Game Developer", "a Software Programmer", "Cloud Engineer", "a Freelancer"],
   description:
     "Creative game developer with one year of experience at India’s leading game studio, with published titles to showcase. Experienced backend developer at IBM Canada's Hybrid Cloud Development team with migrating applications to cloud and proficient in Java, Spring Boot, and Python. Passionate about game mechanics, cloud gaming and problem-solving.",
   github: "https://github.com/patel24kishan",
